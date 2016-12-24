dbc498ba0518e56771f11b90dd7abc4e5fd448ff
Giel van Schijndel <me@mortis.eu>
2016-12-24 22:29:38 +0100

    Always use static analyzer when building with Clang

--- .travis.sh
+++ .travis.sh
@@ -7,7 +7,7 @@ CTEST_OUTPUT_ON_FAILURE=ON
 CMAKE_CXX_STANDARD_REQUIRED=ON
 export CTEST_OUTPUT_ON_FAILURE CMAKE_CXX_STANDARD_REQUIRED
 
-if [ "${ANALYZE}" -a "${CC}" = "clang" ]; then
+if [ "${CC}" = "clang" ]; then
   SCAN_BUILD_CMD="scan-build --use-cc=${CC} --use-c++=${CXX}"
 else
   SCAN_BUILD_CMD=""
@@ -16,7 +16,7 @@ fi
 cmake -E make_directory build
 ${SCAN_BUILD_CMD} cmake -E chdir build cmake -G Ninja ..
 
-if [ "${ANALYZE}" -a "${CC}" != "clang" ]; then
+if [ "${CPPCHECK}" ]; then
   cppcheck --template "{file}:{line}: {severity} ({id}): {message}" \
     --enable=style --force --std=c++11 -j 8 \
     -U__FUNCSIG__ -U_MSC_VER \
@@ -29,7 +29,5 @@ if [ "${ANALYZE}" -a "${CC}" != "clang" ]; then
   fi
 else
   ${SCAN_BUILD_CMD:+${SCAN_BUILD_CMD} --status-bugs} cmake --build build
-fi
-if [ ! "${ANALYZE}" ]; then
   cmake --build build --target test
 fi
