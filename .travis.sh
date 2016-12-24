ef117db7e9f22c865b27d94a70db16cf0c46690b
Giel van Schijndel <me@mortis.eu>
2016-12-24 22:22:42 +0100

    Travis: ensure CppCheck has the export header

--- .travis.sh
+++ .travis.sh
@@ -9,20 +9,21 @@ else
   SCAN_BUILD_CMD=""
 fi
 
+cmake -E make_directory build
+${SCAN_BUILD_CMD} cmake -E chdir build cmake -G Ninja ..
+
 if [ "${ANALYZE}" -a "${CC}" != "clang" ]; then
   cppcheck --template "{file}:{line}: {severity} ({id}): {message}" \
     --enable=style --force --std=c++11 -j 8 \
     -U__FUNCSIG__ -U_MSC_VER \
     -U__GNUC__ -U__clang__ \
-    -Iinclude \
+    -Iinclude -Ibuild/include/awesome \
     src 2> build/cppcheck.txt
   if [ -s build/cppcheck.txt ]; then
     cat < build/cppcheck.txt >&2
     exit 1
   fi
 else
-  cmake -E make_directory build
-  ${SCAN_BUILD_CMD} cmake -E chdir build cmake -G Ninja ..
   ${SCAN_BUILD_CMD:+${SCAN_BUILD_CMD} --status-bugs} cmake --build build
 fi
 if [ ! "${ANALYZE}" ]; then
