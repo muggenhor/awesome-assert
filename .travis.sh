cbdba2323872473e6ec779b292f580f4e40de4c6
Giel van Schijndel <me@mortis.eu>
2016-12-24 15:55:28 +0100

    Add static analysis

---
+++ .travis.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+set -e
+set -x
+
+cmake -E make_directory build
+if [ "${ANALYZE}" ]; then
+  if [ "${CC}" = "clang" ]; then
+    scan-build cmake -E chdir build cmake -G Ninja ..
+    scan-build --status-bugs cmake --build build
+  else
+    cppcheck --template "{file}:{line}: {severity} ({id}): {message}" \
+      --enable=style --force --std=c++11 -j 8 \
+      -I include src 2> build/cppcheck.txt
+    if [ -s build/cppcheck.txt ]; then
+      cat < build/cppcheck.txt >&2
+      exit 1
+    fi
+  fi
+else
+  cmake -E chdir build cmake -G Ninja ..
+  cmake --build build
+  cmake --build build --target test
+fi
