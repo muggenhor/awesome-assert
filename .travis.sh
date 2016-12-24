ac91389939914bb1e0d43674520786a196c11468
Giel van Schijndel <me@mortis.eu>
2016-12-24 22:21:05 +0100

    Travis: share cmake commands between regular and analyzer builds

--- .travis.sh
+++ .travis.sh
@@ -3,25 +3,28 @@
 set -e
 set -x
 
-cmake -E make_directory build
-if [ "${ANALYZE}" ]; then
-  if [ "${CC}" = "clang" ]; then
-    scan-build --use-cc="${CC}" --use-c++="${CXX}" cmake -E chdir build cmake -G Ninja ..
-    scan-build --use-cc="${CC}" --use-c++="${CXX}" --status-bugs cmake --build build
-  else
-    cppcheck --template "{file}:{line}: {severity} ({id}): {message}" \
-      --enable=style --force --std=c++11 -j 8 \
-      -U__FUNCSIG__ -U_MSC_VER \
-      -U__GNUC__ -U__clang__ \
-      -Iinclude \
-      src 2> build/cppcheck.txt
-    if [ -s build/cppcheck.txt ]; then
-      cat < build/cppcheck.txt >&2
-      exit 1
-    fi
+if [ "${ANALYZE}" -a "${CC}" = "clang" ]; then
+  SCAN_BUILD_CMD="scan-build --use-cc=${CC} --use-c++=${CXX}"
+else
+  SCAN_BUILD_CMD=""
+fi
+
+if [ "${ANALYZE}" -a "${CC}" != "clang" ]; then
+  cppcheck --template "{file}:{line}: {severity} ({id}): {message}" \
+    --enable=style --force --std=c++11 -j 8 \
+    -U__FUNCSIG__ -U_MSC_VER \
+    -U__GNUC__ -U__clang__ \
+    -Iinclude \
+    src 2> build/cppcheck.txt
+  if [ -s build/cppcheck.txt ]; then
+    cat < build/cppcheck.txt >&2
+    exit 1
   fi
 else
-  cmake -E chdir build cmake -G Ninja ..
-  cmake --build build
+  cmake -E make_directory build
+  ${SCAN_BUILD_CMD} cmake -E chdir build cmake -G Ninja ..
+  ${SCAN_BUILD_CMD:+${SCAN_BUILD_CMD} --status-bugs} cmake --build build
+fi
+if [ ! "${ANALYZE}" ]; then
   cmake --build build --target test
 fi
