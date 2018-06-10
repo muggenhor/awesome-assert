cc99fef8abf9fd9c61b40447659968d237e5d1b2
Giel van Schijndel <me@mortis.eu>
2018-06-10 16:57:44 +0200

    Travis: build with GCC 4.7 and 5

--- .travis.sh
+++ .travis.sh
@@ -7,19 +7,15 @@ CTEST_OUTPUT_ON_FAILURE=ON
 CMAKE_CXX_STANDARD_REQUIRED=ON
 export CTEST_OUTPUT_ON_FAILURE CMAKE_CXX_STANDARD_REQUIRED
 
-if [ "${CC}" = "gcc" ] && which gcc-4.7 > /dev/null 2>&1; then
-  CC=gcc-4.7
-fi
-if [ "${CXX}" = "g++" ] && which g++-4.7 > /dev/null 2>&1; then
-  CXX=g++-4.7
-fi
-
 if [ "${CC}" = "clang" -a "${TRAVIS_OS_NAME}" != "osx" ]; then
   SCAN_BUILD_CMD="scan-build --use-cc=${CC} --use-c++=${CXX}"
 fi
 
 cmake -E make_directory build
-${SCAN_BUILD_CMD} cmake -E chdir build cmake -G Ninja ..
+${SCAN_BUILD_CMD} cmake -E chdir build cmake \
+  -G Ninja \
+  ${CMAKE_CXX_COMPILER:+"-DCMAKE_CXX_COMPILER=${CMAKE_CXX_COMPILER}"} \
+  ..
 
 if [ "x${CPPCHECK:-}" = "xyes" ]; then
   cppcheck --template "{file}:{line}: {severity} ({id}): {message}" \
